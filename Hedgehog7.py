a52100b6988926ac35583960e8b251c61eff4b2f
Jeff Diamond (Radecki) <jeffryscottdiamond@gmail.com>
2026-08-01 19:32:53 -0500

    Update Hedgehog7.py

--- Hedgehog7.py
+++ Hedgehog7.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as ui_chart
 # ---------------------------------------------------------
 st.set_page_config(page_title="The Hedgehog 7 Sandbox", layout="wide")
 st.title("Mag 7 🦊 Foxes vs. the 🦔 Hedgehog 7: Macro Regime Performance Sandbox")
-st.markdown("### The Magnificient Seven, chasing rabbits vs. doing one thing well.")
+st.markdown("### The Magnificient Seven vs. Defensive Growth. A.k.a., chasing rabbits vs. doing one thing well.")
 st.caption("Designed by Jeff Diamond-Radecki, June 30, 2026")
 st.write("Testing the Magnificent 7 against my low-volatility, dividend-compounding alternative index. Inspired by the principles set forth by Jim Collins in his book, *Good to Great.*")
 
